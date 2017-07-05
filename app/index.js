4fce3a21cb55f9da11e32c7065e16360689ea0b8
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-07-05 14:03:50 -0400

    initial commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+var styles = require('./index.styles');
+
+export default class Kicks4LoveApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
---
+++ index.android.js
@@ -0,0 +1,4 @@
+import Index from './app/indexpp';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('Kicks4LoveApp', () => Index);
