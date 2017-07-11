008806928e8a5e539f3d7d6e800db2886327579c
ZujuSun <sunzuju@gmail.com>
2017-07-11 17:09:48 -0400

    http request get and render

--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image, ScrollView, Dimensions } from 'react-native';
+import { ActivityIndicator,View, Text, Image, ScrollView, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Swiper from 'react-native-swiper';
@@ -8,33 +8,59 @@ const indexStyles = require('../styles/index.styles');
 const width = Dimensions.get('window').width;
 
 export default class Index extends Component {  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataRecord: [],
+    }
+  }
+
+  componentDidMount() {
+    return fetch('https://4f2f9aed.ngrok.io/api/v0/home_posts?next_page=2&l=cn')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        this.setState({ 
+          isLoading:false,
+          dataRecord: responseJson.posts, 
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
 	build() {
-		// TODO: make build built from api call
+
+    var contents = this.state.dataRecord.map(function (item){
+      return (
+          <View key={item.post.id} style={ indexStyles.slide }>
+            <Image source={{uri: item.image_url}} style={indexStyles.slideImage}>
+                <View style={indexStyles.slideImageInnerFrame}/>
+            </Image>
+            <Text style={indexStyles.text}>{item.post.title}</Text>
+          </View>
+      );
+    });
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
 
 		return (
 			<Swiper showsButtons={true} autoplay={true} height={300} activeDotColor={'#fff'} paginationStyle={indexStyles.sliderPagination}
 				nextButton={<Icon name="chevron-right" style={{color: '#fff', fontSize: 28}} />}
 				prevButton={<Icon name="chevron-left" style={{color: '#fff', fontSize: 28}} />}
 			>
-        		<View style={indexStyles.slide}>
-        			<Image source={{uri: 'https://kicks4love.com/uploads/post/15/Curry3_-5.jpg'}} style={indexStyles.slideImage}>
-        				<View style={indexStyles.slideImageInnerFrame}/>
-        			</Image>
-          			<Text style={indexStyles.text}>Ten Top most memorable Curry 3s in This Season</Text>
-        		</View>
-        		<View style={indexStyles.slide}>
-        			<Image source={{uri: 'https://kicks4love.com/uploads/post/20/foam.jpg'}} style={indexStyles.slideImage}>
-        				<View style={indexStyles.slideImageInnerFrame}/>
-        			</Image>
-          			<Text style={indexStyles.text}>Top 4 must-knows about Foamposite</Text>
-        		</View>
-        		<View style={indexStyles.slide}>
-        			<Image source={{uri: 'https://kicks4love.com/uploads/post/23/YezzyDGreen.jpg'}} style={indexStyles.slideImage}>
-        				<View style={indexStyles.slideImageInnerFrame}/>
-        			</Image>
-          			<Text style={indexStyles.text}>Adidas Yezzy 350 "Dark Green" will be release next month</Text>
-        		</View>
-      		</Swiper>
+        { contents }
+      </Swiper>
 		);
 	}
 
