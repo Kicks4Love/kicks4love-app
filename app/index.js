861f5486bd2426141bc3da6b10aa805c39af7697
ZujuSun <sunzuju@gmail.com>
2017-07-11 14:32:48 -0400

    feature added

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import {ActivityIndicator, Text, View, Button, Image, TouchableOpacity } from 'react-native';
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
 import Drawer from './components/Drawer';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -9,6 +9,18 @@ import Features from './features';
 const styles = require('./styles/index.styles');
 
 class Index extends Component {
+
+  componentWillMount() {
+    fetch('https://4f2f9aed.ngrok.io/api/v0/featured_posts?next_page=1')
+      .then((response) => response.json())
+      .then((responseJson) => { 
+        this.setState({
+          dataRecord: responseJson.posts,
+          isLoading: false
+        })
+    });
+  }
+
   // TODO: extract navigationOptions to a helper method
   static navigationOptions = props => ({ 
     title: 'Home',
@@ -23,14 +35,25 @@ class Index extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dataRecord:[],
+      isLoading: true
+    };
   }
   
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to Kicks4Love Mobile App!
+          Welcome to Kicks4Love Mobile App! {this.state.dataRecord.posts}
         </Text>
       </View>
     );
