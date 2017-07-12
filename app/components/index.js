45bdec904f7478ccae53069973495f1c955d8b12
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-07-12 13:48:27 -0400

    make api call on home page work, with only the first page right not

--- app/components/index.js
+++ app/components/index.js
@@ -1,51 +1,30 @@
 import React, { Component } from 'react';
-import { ActivityIndicator,View, Text, Image, ScrollView, Dimensions } from 'react-native';
+import { ActivityIndicator, Alert, View, Text, Image, ScrollView, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 import Swiper from 'react-native-swiper';
 
 const indexStyles = require('../styles/index.styles');
 const width = Dimensions.get('window').width;
 
 export default class Index extends Component {  
-
   constructor(props) {
     super(props);
-    this.state = {
-      isLoading: true,
-      dataRecord: [],
-    }
+    this.state = { isLoading: true, sliderRecord: [], postRecord: [] }
   }
 
   componentDidMount() {
-    return fetch('https://4f2f9aed.ngrok.io/api/v0/home_posts?next_page=2&l=cn')
+    return fetch('https://f0d6e4fc.ngrok.io/api/v0/home_posts?next_page=1')
       .then((response) => response.json())
       .then((responseJson) => {
-        console.log(responseJson);
         this.setState({ 
-          isLoading:false,
-          dataRecord: responseJson.posts, 
-        })
-      })
-      .catch((error) => {
-        console.error(error);
+          isLoading: false,
+          sliderRecord: responseJson.slider_posts, 
+          postRecord: responseJson.posts
+        });
       });
   }
 
-
 	build() {
-
-    var contents = this.state.dataRecord.map(function (item){
-      return (
-          <View key={item.post.id} style={ indexStyles.slide }>
-            <Image source={{uri: item.image_url}} style={indexStyles.slideImage}>
-                <View style={indexStyles.slideImageInnerFrame}/>
-            </Image>
-            <Text style={indexStyles.text}>{item.post.title}</Text>
-          </View>
-      );
-    });
-
     if (this.state.isLoading) {
       return (
         <View style={{flex: 1, paddingTop: 20}}>
@@ -54,23 +33,69 @@ export default class Index extends Component {
       );
     }
 
+    let slider = this.state.sliderRecord.map(function (item){
+      return (
+        <View key={item.post_type + '/' + item.post.id} style={indexStyles.slide}>
+          <Image source={{uri: item.image_url}} style={indexStyles.slideImage}>
+            <View style={indexStyles.slideImageInnerFrame}/>
+          </Image>
+          <Text style={indexStyles.slideText}>{item.post.title}</Text>
+        </View>
+      );
+    });
+
+    let posts = this.state.postRecord.map(function (item){
+      let tag = '';
+      switch (item.post_type) {
+        case 'features':
+          tag = 'Features';
+          break;
+        case 'trend':
+          tag = 'Trend';
+          break;
+        case 'oncourt':
+          tag = 'On Court';
+          break;
+        case 'streetsnap':
+          tag = 'Street Snap';
+          break;
+        case 'rumors':
+          tag = 'Rumors';
+          break;
+      }
+      return (
+        <View key={item.post_type + '/' + item.post.id} style={indexStyles.box}>
+          <Image source={{uri: item.image_url, width: width * 0.4, height: 100}} style={indexStyles.coverImage} />
+          <View style={indexStyles.boxContent}>
+            <Text style={indexStyles.boxTitle}>{item.post.title} <Text style={indexStyles.boxDate}>{item.post.created_at.slice(0, 10)}</Text></Text>
+            <Text style={indexStyles.boxPostType}><Icon name="tags" /><Text style={indexStyles.boxPostTypeText}>{tag}</Text></Text>
+            <Text style={indexStyles.boxRate}>{item.score}/5.0 <Image source={require('../images/sneakerblack.png')} style={indexStyles.boxRateImage} /></Text>
+          </View>
+        </View>
+      );
+    });
+
 		return (
-			<Swiper showsButtons={true} autoplay={true} height={300} activeDotColor={'#fff'} paginationStyle={indexStyles.sliderPagination}
-				nextButton={<Icon name="chevron-right" style={{color: '#fff', fontSize: 28}} />}
-				prevButton={<Icon name="chevron-left" style={{color: '#fff', fontSize: 28}} />}
-			>
-        { contents }
-      </Swiper>
+      <View>
+  			<Swiper showsButtons={true} autoplay={true} height={300} activeDotColor={'#fff'} paginationStyle={indexStyles.sliderPagination}
+  				nextButton={<Icon name="chevron-right" style={{color: '#fff', fontSize: 28}} />}
+  				prevButton={<Icon name="chevron-left" style={{color: '#fff', fontSize: 28}} />}
+  			>
+          {slider}
+        </Swiper>
+        <Text style={indexStyles.logan}><Icon name="check" /> Using Kicks4Love App, Better Experience</Text>
+        {posts}
+      </View>
 		);
 	}
 
 	render() {
-		let Content = this.build();
+		let content = this.build();
 
-	    return (
-	      	<ScrollView>
-	        	{Content}
-	      	</ScrollView>
-	    );
+	  return (
+	    <ScrollView>
+	      {content}
+	    </ScrollView>
+	  );
 	}
 }
